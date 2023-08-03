bb65a18d83fcf28e0ef34f0726b1a7bd10ee5b29
PeymanOruji <peiman.oruji@gmail.com>
2023-08-03 12:21:28 +0330

    Fix not used variable issue

--- src/components/kit/Input.tsx
+++ src/components/kit/Input.tsx
@@ -6,7 +6,7 @@ export default function Input(props: InputType) {
     const {type, placeholder, value, onChange} = props;
     return <div className={classes.wrapper}>
         <img className={classes.icon} src="/searchicon.png" alt="searchIcon"/>
-        <input className={classes.input} type={type} placeholder={placeholder} onChange={(e) => onChange(e)}/>
+        <input className={classes.input} value={value} type={type} placeholder={placeholder} onChange={(e) => onChange(e)}/>
     </div>
 }
 
