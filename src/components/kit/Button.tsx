1c664de5ea2f06021a911c846f65e542716b113b
PeymanOruji <peiman.oruji@gmail.com>
2023-08-03 12:31:13 +0330

    Remove extra line

--- src/components/kit/Button.tsx
+++ src/components/kit/Button.tsx
@@ -1,7 +1,6 @@
 import {ButtonType} from './Button.types'
 import classes from './Button.module.scss';
 
-
 export default function Button(props: ButtonType) {
     const {action, text} = props;
     return <button className={classes.button} onClick={() => action()}>
