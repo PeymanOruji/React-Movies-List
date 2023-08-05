f6a01492dfca0b27e1f13df7611301ff91b750cd
PeymanOruji <peiman.oruji@gmail.com>
2023-08-05 15:40:46 +0330

    Add StoryBook for Nav component
    Add StoryBook for Nav component

---
+++ src/components/Nav.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Nav from './Nav'
+
+export default {
+    title: 'Form/Nav',
+    component: Nav
+}
+
+export const NavBar = () => <Nav search="" onSearch={() => null}/>
